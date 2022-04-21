0a8c2c64184cb4379eda36f9d68a11554b807650
Colin Bruner <colin.d.bruner@gmail.com>
2022-04-21 16:42:12 -0400

    refactor this a bit using OOP

--- refresh.py
+++ refresh.py
@@ -16,178 +16,199 @@ ec2 = boto3.resource("ec2")
 min_terminate_seconds = 120
 
 
-# Headers to pass for authentication
-headers = {
-    "consul": {"X-Consul-Token": os.environ.get("CONSUL_HTTP_TOKEN")},
-    "nomad": {"X-Nomad-Token": os.environ.get("NOMAD_TOKEN")},
-    "vault": {"X-Vault-Token": os.environ.get("VAULT_TOKEN")},
-}
-# Environment variables for system tokens
-env = {
-    "consul": {"ADDR": "CONSUL_HTTP_ADDR", "TOKEN": "CONSUL_HTTP_TOKEN"},
-    "vault": {"ADDR": "VAULT_ADDR", "TOKEN": "VAULT_TOKEN"},
-    "nomad": {"ADDR": "NOMAD_ADDR", "TOKEN": "NOMAD_TOKEN"},
-}
+class System:
+    # Headers to pass for authentication
+    _headers = {
+        "consul": {"X-Consul-Token": os.environ.get("CONSUL_HTTP_TOKEN")},
+        "nomad": {"X-Nomad-Token": os.environ.get("NOMAD_TOKEN")},
+        "vault": {"X-Vault-Token": os.environ.get("VAULT_TOKEN")},
+    }
+    # Environment variables for system tokens
+    _env = {
+        "consul": {"ADDR": "CONSUL_HTTP_ADDR", "TOKEN": "CONSUL_HTTP_TOKEN"},
+        "vault": {"ADDR": "VAULT_ADDR", "TOKEN": "VAULT_TOKEN"},
+        "nomad": {"ADDR": "NOMAD_ADDR", "TOKEN": "NOMAD_TOKEN"},
+    }
 
-endpoints = {
-    "consul": "/v1/operator/raft/configuration",
-    "nomad": "/v1/operator/raft/configuration",
-    "vault": "/v1/sys/storage/raft/configuration",
-}
+    _api_raft_config = {
+        "consul": "/v1/operator/raft/configuration",
+        "nomad": "/v1/operator/raft/configuration",
+        "vault": "/v1/sys/storage/raft/configuration",
+    }
+    _api_raft_remove = {"vault": "/v1/sys/storage/raft/remove-peer", "consul": "", "nomad": ""}
 
-keymap = {
-    "consul": {"node": "Node", "leader": "Leader", "voter": "Voter"},
-    "nomad": {"node": "Node", "leader": "Leader", "voter": "Voter"},
-    "vault": {"node": "node_id", "leader": "leader", "voter": "voter"},
-}
+    _keymap = {
+        "consul": {"node": "Node", "leader": "Leader", "voter": "Voter"},
+        "nomad": {"node": "Node", "leader": "Leader", "voter": "Voter"},
+        "vault": {"node": "node_id", "leader": "leader", "voter": "voter"},
+    }
 
+    def __init__(self, system):
+        self.name = system
+        # API
+        self.address = os.environ.get(self._env[system]["ADDR"])
+        self.headers = self._headers[system]
 
-# class Peer:
-#    def __init__(self, system):
-#        self.system = system
-#        self.headers = headers[system]
-#
-#    def raft_config(self):
-#        return requests.get(system_url, headers=self.headers).json()
+        # Sanity
+        self.address_name = self._env[self.name]["ADDR"]
+        self.token_name = self._env[self.name]["TOKEN"]
 
+        # Shortcuts for accessing keys within returned `get_peers()` data.
+        self.node_key = self._keymap[system]["node"]
+        self.leader_key = self._keymap[system]["leader"]
+        self.voter_key = self._keymap[system]["voter"]
 
-def preflight_sanity():
-    """Verify token / address are in place before continuing."""
-    # TODO: Preflight auth test, correct token?
+        ###
+        # API Access URLs
+        ###
+        self.raft_config_url = self.address + self._api_raft_config[system]
+        # Only implemented for vault currently.
+        self.raft_delete_url = self.address + self._api_raft_remove[system]
 
-    # .values() returns a 'view', need to cast to list to determine if any elements exist
-    if not list(headers[system].values())[0]:
-        print(f"Token for {system} is not defined. Please export {tokens[system]} and try again.")
-        sys.exit(1)
-    else:
-        print(f"Success. Token for {system} found with environment variables.")
+    def preflight_checks(self):
+        """Verify token / address are in place before continuing."""
+        # TODO: Preflight auth test, correct token?
 
-    # Remove any 'https://' protocol strings, remove any ending (e.g.) ':8200' port strings
-    if ":" in system_addr:
-        system_hostname = system_addr.split("://")[1].split(":")[0]
-
-    if not socket.gethostbyname(system_hostname):
-        print(f"Unable to resolve {system_hostname} based off environment variables.")
-        sys.exit(2)
-    else:
-        print(f"Success. Able to resolve {system_hostname} address.")
-
-    # Final check print account ID
-    print("Checking authentication against AWS.")
-    print(
-        f"Success. Will continue under AccountID {boto3.client('sts').get_caller_identity()['Account']}"
-    )
-    return True
-
-
-def get_peers():
-    """Fetch all node JSON via systems API"""
-    data = requests.get(system_url, headers=headers[system]).json()
-    if system == "consul" or system == "nomad":
-        print(system)
-        return data["Servers"]
-    elif system == "vault":
-        return data["data"]["config"]["servers"]
-
-
-def check_cluster_peers(waiting=bool) -> bool:
-    """Check if the current total of peers equals the original total number of peers"""
-    current_peers = get_peers()
-    current_total_peers = len(current_peers)
-
-    if current_total_peers == total_peers:
-        # Original number of peers are back.
-        print(
-            f"Determined {current_total_peers} peers out of an expected total: {total_peers} peers have joined the cluster."
-        )
-
-        # We need Voter = True on the last instance to be included in the cluster. Check last element of list to confirm.
-        # TODO: This is not authorative, I don't know if the order will be the same across systems and this could fail.
-        if not current_peers[-1][voter_key]:
-            print(f"Peer has not registered as a voter, sleeping for 5s")
-            time.sleep(5)
-            return True
+        # Check that an address has been provided within environment variables.
+        if not self.address:
+            print(f"ERROR: Address for {self.name} is not defined. Please export {self.address_name} and try again.")
+            sys.exit(1)
         else:
-            print(f"Peer has synced raft data successfully, continuing.")
-            return False
-    else:
+            print(f"Success: Address {self.address} for {self.name} found within environment variables.")
+
+        # Check that a token has been provided within environment variables.
+        # .values() returns a 'view', need to cast to list to determine if any elements exist
+        if not os.environ.get(self.token_name):
+            print(f"ERROR: Token for {self.name} is not defined. Please export {self.token_name} and try again.")
+            sys.exit(1)
+        else:
+            print(f"Success: Token for {self.name} found with environment variables.")
+
+        # In order to validate we can resolve the hostname:
+        # remove any 'https://' protocol strings,
+        # remove any ending (e.g.) ':8200' port strings
+        if ":" in self.address:
+            system_hostname = self.address.split("://")[1].split(":")[0]
+
+        try:
+            socket.gethostbyname(system_hostname)
+        except socket.gaierror:
+            print(f"ERROR: Unable to resolve {system_hostname}.")
+            sys.exit(2)
+        else:
+            print(f"Success: Resolved {system_hostname} address.")
+
+        # Final check print account ID
         print(
-            f"Sleeping for 10s to wait for a total of {total_peers} {system.title()} Servers to join the cluster."
+            f"Success: Authenticated to AWS. Will continue under AccountID {boto3.client('sts').get_caller_identity()['Account']}"
         )
-        time.sleep(10)
-        return True
+
+    def get_raft_config(self) -> list:
+        """Gets raft configuration for a cluster."""
+        data = requests.get(self.raft_config_url, headers=self.headers).json()
+        if self.name == "consul" or self.name == "nomad":
+            return data["Servers"]
+        elif self.name == "vault":
+            return data["data"]["config"]["servers"]
+
+    def check_cluster_peers(self, waiting=bool) -> bool:
+        """Check if the current total of peers equals the original total number of peers"""
+        current_peers = self.get_raft_config()
+        current_total_peers = len(current_peers)
+
+        if current_total_peers == total_peers:
+            # Original number of peers are back.
+            print(
+                f"Determined {current_total_peers} peers out of an expected total: {total_peers} peers have joined the cluster."
+            )
+
+            # We need Voter = True on the last instance to be included in the cluster. Check last element of list to confirm.
+            # TODO: This is not necessarily authorative, I don't know if the order will be the same across systems so this could fail?
+            if not current_peers[-1][self.voter_key]:
+                print(f"Peer has not registered as a voter, sleeping for 5s")
+                time.sleep(5)
+                return True
+            else:
+                print(f"Peer has registered as a voter and synced raft data successfully, continuing.")
+                return False
+        else:
+            print(
+                f"Sleeping for 10s to wait for a total of {total_peers} {self.name.title()} Servers to join the cluster."
+            )
+            time.sleep(10)
+            return True
+
+    def remove_peer(self, peer=dict):
+        """
+        Removes a Peer from Vaults raft configuration.
+        This currently only applies to Vault systems.
+        """
+        if self.name != "vault":
+            print(f"ERROR: Removing a peer from raft configuration is not implemented for {self.name.title()}")
+            raise (NotImplemented)
+
+        # Remove a peer by 'server_id', its node identifier
+        node = peer[self.node_key]
+
+        data = requests.post(self.raft_delete_url, data={"server_id": node}, headers=self.headers)
+        # Vault returns a 204, 'No Content'. Not really sure how to error handle this, so just exiting.
+        if data.status_code == 204:
+            print(f"Successfully removed {node} from {self.name.title()}'s raft configuration.")
+        else:
+            print(f"ERROR Unable to remove {node} from raft. Exiting now, please review.")
+            sys.exit(3)
+
+    def terminate_peer(self, peer=dict):
+        """
+        Terminate an EC2 instance by instance ID.
+        returns: datetime object representing time since last terminated
+        """
+        # Nomad is special, and will include '.region' aloong with Node ID e.g. 'i-0b9efdb21a715536f.us-east-1'
+        node = peer[self.node_key].split(".")[0]
+        leader = peer[self.leader_key]
+
+        print(f"Cycling Node: {node} -- Leader: {leader}")
+        ec2.instances.filter(InstanceIds=[node]).terminate()
+        print(f"Successfully Terminated Instance")
+
+        # Returns the datetime representation of last terminated instance
+        return datetime.now()
+
+    def safety_pause(self, terminated_time):
+        """Prevent any node from being terminated earlier than 'min_terminate_seconds'"""
+        seconds_since_last_terminated = int()
+
+        # Ensure least 2minutes before terminating a 2nd instance
+        while seconds_since_last_terminated < min_terminate_seconds:
+            print(
+                f"Terminated last instance {seconds_since_last_terminated} seconds ago. Waiting at least {min_terminate_seconds} before checking cluster status."
+            )
+            time.sleep(10)
+
+            # Calculate how long its been since the last instance was terminated.
+            seconds_since_last_terminated = (datetime.now() - terminated_time).seconds
 
 
-def remove_peer(peer=dict):
-    """
-    Removes a Peer from Vaults raft configuration.
-    This currently only applies to Vault systems.
-    """
-    # Remove a peer by 'server_id', its node identifier
-    node = peer[node_key]
-
-    remove_peer_endpoint = "/v1/sys/storage/raft/remove-peer"
-    remove_peer_url = system_addr + remove_peer_endpoint
-    data = requests.post(remove_peer_url, data={"server_id": node}, headers=headers[system])
-    # Vault returns a 204, 'No Content'. Not really sure how to error handle this, so just exiting.
-    if data.status_code == 204:
-        print(f"Successfully removed {node} from {system.title()}'s raft configuration.")
-    else:
-        print(f"ERROR Unable to remove {node} from raft. Exiting now, please review.")
-        sys.exit(3)
-
-
-def terminate_peer(peer=dict):
-    """
-    Terminate an EC2 instance by instance ID.
-    returns: datetime object representing time since last terminated
-    """
-    # Nomad is special, and will include '.region' aloong with Node ID e.g. 'i-0b9efdb21a715536f.us-east-1'
-    node = peer[node_key].split(".")[0]
-    leader = peer[leader_key]
-
-    print(f"Cycling Node: {node} -- Leader: {leader}")
-    ec2.instances.filter(InstanceIds=[node]).terminate()
-    print(f"Successfully Terminated Instance")
-
-    # Returns the datetime representation of last terminated instance
-    return datetime.now()
-
-
-def safety_pause(terminated_time):
-    """Prevent any node from being terminated earlier than 'min_terminate_seconds'"""
-    seconds_since_last_terminated = int()
-
-    # Ensure least 2minutes before terminating a 2nd instance
-    while seconds_since_last_terminated < min_terminate_seconds:
-        print(
-            f"Terminated last instance {seconds_since_last_terminated} seconds ago. Waiting at least {min_terminate_seconds} before checking cluster status."
-        )
-        time.sleep(10)
-
-        # Calculate how long its been since the last instance was terminated.
-        seconds_since_last_terminated = (datetime.now() - terminated_time).seconds
-
-
-def main():
+def main(system: System, peers=list, total_peers=int):
     print(f"Calculated {total_peers} peers exist within the cluster.")
+
     for peer in peers:
         waiting = True
 
         # Ensure cluster has its initial total number of peers
         while waiting:
-            waiting = check_cluster_peers(waiting)
+            waiting = system.check_cluster_peers(waiting)
 
         # Vault does not set 'cleanup_dead_servers' to True by default. When cycling peers, they must
         # be manually cleaned up unless this autopilot setting is enabled.
-        if system == "vault":
-            remove_peer(peer)
+        if system.name == "vault":
+            system.remove_peer(peer)
 
         # Terminate the instance
-        terminated_time = terminate_peer(peer)
+        terminated_time = system.terminate_peer(peer)
 
         # Prevent terminations sooner than 'min_terminate_seconds'
-        safety_pause(terminated_time)
+        system.safety_pause(terminated_time)
 
     print(f"Refreshed {total_peers} successfully.")
 
@@ -206,31 +227,22 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Working with only a single system, this will be our pointer
-    system = args.system
-
-    system_addr = os.environ.get(env[system]["ADDR"])
-    # Hostname + API URI
-    system_url = system_addr + endpoints[system]
-
-    # Shortcuts for accessing keys within returned `get_peers()` data.
-    node_key = keymap[system]["node"]
-    leader_key = keymap[system]["leader"]
-    voter_key = keymap[system]["voter"]
+    system = System(args.system)
 
     ###
     # Sanity
     ###
-    # Make sure tokens are set
-    preflight_sanity()
+    # Run preflight checks before beginning
+    system.preflight_checks()
 
     ###
     # Initial Values
     ###
     # Total Peers
-    peers = get_peers()
+    peers = system.get_raft_config()
     # Move 'Leader' to end of the list
-    peers.sort(key=lambda x: x[leader_key] == True)
+    peers.sort(key=lambda x: x[system.leader_key] == True)
     # Initial Total Peers count
     total_peers = len(peers)
 
-    main()
+    main(system, peers, total_peers)
